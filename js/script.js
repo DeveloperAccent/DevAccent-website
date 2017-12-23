1ac01cc38d83f521ca80eb18c5d35bad5d3da31f
Somiari Lucky <somiarilucky@gmail.com>
2017-12-23 16:37:03 +0100

    Fix some more send-love stuff

--- js/script.js
+++ js/script.js
@@ -9,8 +9,8 @@ ajax.onerror = failedAttempt;
 ajax.open("GET", "https://api.netlify.com/api/v1/forms/5a3e7682494c5852683421de?access_token=976354873ebb7e74404f9fabe81e493ca236627a1d95c91b92577428aee803fe", true);
 ajax.send();
 
-function getSubmissionCount() {
-	console.log(this); // log the response
+function getSubmissionCount(e) {
+	e.preventDefault();
 	if (this.status == 200) { // request succeeded
 		// do something with this.responseText;
 		var json = JSON.parse(this.responseText);
