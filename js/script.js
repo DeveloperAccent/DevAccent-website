a20871a8e752c54a2fbca66f87107d89274822b1
Somiari Lucky <somiarilucky@gmail.com>
2017-12-23 16:29:34 +0100

    Add 'send love' feature

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const loveCount = document.querySelector('.love-count');
+const submitLove = document.querySelector('.send-love-btn');
+
+submitLove.addEventListener('click', getSubmissionCount);
+
+var ajax = new XMLHttpRequest();
+ajax.onload = getSubmissionCount;
+ajax.onerror = failedAttempt;
+ajax.open("GET", "https://api.netlify.com/api/v1/forms/5a3974998c8f33460027a0e1?access_token=976354873ebb7e74404f9fabe81e493ca236627a1d95c91b92577428aee803fe", true);
+ajax.send();
+
+function getSubmissionCount() {
+	console.log(this); // log the response
+	if (this.status == 200) { // request succeeded
+		// do something with this.responseText;
+		var json = JSON.parse(this.responseText);
+		var subCount = json.submission_count;
+		loveCount.innerText = subCount;
+	} else {
+		// handle more HTTP response codes here;
+	}
+}
+
+function failedAttempt(e) {
+	console.log(this);
+	console.error(e);
+	// do something with this.status, this.statusText
+}
