f153ed3b5c17af92bf3cd974cb9252da97ba9019
Somiari Lucky <somiarilucky@gmail.com>
2017-12-24 09:33:06 +0100

    Test ajax form submit

--- js/script.js
+++ js/script.js
@@ -14,9 +14,9 @@ function sendForm(e) {
 	e.preventDefault();
 		showLoveForm.innerHTML += "<input type='hidden' name='form-name' value='love' />";
 		const url = showLoveForm.getAttribute('action');
-		ajax.open('POST', url, true);
+		ajax.open('GET', 'index.html', true);
 		ajax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-		ajax.send('');
+		ajax.send();
 }
 
 function getSubmissionCount() {
