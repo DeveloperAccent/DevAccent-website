c542ad6a871dd674e866f8f13c5314063e42d657
Somiari Lucky <somiarilucky@gmail.com>
2017-12-24 04:42:43 +0100

    Test ajax form submit

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function submitForm(e) {
 		ajax.onreadystatechange = function() {//Call a function when the state changes.
 			if(http.readyState == 4 && http.status == 200) {
 					alert(http.responseText);
-			}
+			}}
 		ajax.send();
 }
 
