dc4c31b6c83cd8fc63c19e913e9c09016f7eb31d
Somiari Lucky <somiarilucky@gmail.com>
2017-12-24 05:16:04 +0100

    Test ajax form submit and retrieve

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const loveCount = document.querySelector('.love-count');
 const submitLove = document.querySelector('.send-love-btn');
 const showLoveForm = document.querySelector('.show-love-form');
 
-// submitLove.addEventListener('click', sendForm);
+submitLove.addEventListener('click', sendForm);
 
 var ajax = new XMLHttpRequest();
 ajax.onload = getSubmissionCount;
@@ -10,21 +10,20 @@ ajax.onerror = failedAttempt;
 ajax.open("GET", "https://api.netlify.com/api/v1/forms/5a3e7682494c5852683421de?access_token=976354873ebb7e74404f9fabe81e493ca236627a1d95c91b92577428aee803fe", true);
 ajax.send();
 
-// function sendForm(e) {
-// 	e.preventDefault();
-// 		showLoveForm.innerHTML += "<input type='hidden' name='form-name' value='love' />";
-// 		const url = showLoveForm.getAttribute('action');
-// 		ajax.open('POST', 'url', true);
-// 		ajax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-// 		ajax.send(null);
-// 		console.log("sendForm" + this);
-// }
+function sendForm(e) {
+	e.preventDefault();
+		showLoveForm.innerHTML += "<input type='hidden' name='form-name' value='love' />";
+		const url = showLoveForm.getAttribute('action');
+		ajax.open('POST', 'url', true);
+		ajax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+		ajax.send();
+}
 
 function getSubmissionCount() {
 	console.log("getSub" + this);
-	if (XMLHttpRequest.status == 200) { // request succeeded
+	if (this.status == 200) { // request succeeded
 		// do something with this.responseText;
-		var json = JSON.parse(XMLHttpRequest.responseText);
+		var json = JSON.parse(this.responseText);
 		var subCount = json.submission_count;
 		loveCount.innerText = subCount;
 	} else {
