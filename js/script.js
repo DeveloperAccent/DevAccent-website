1604fc5e2c07a6b82503339822362672c955b50f
Somiari Lucky <somiarilucky@gmail.com>
2017-12-24 03:30:39 +0100

    Test ajax form submit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
 const loveCount = document.querySelector('.love-count');
 const submitLove = document.querySelector('.send-love-btn');
+const showLoveForm = document.querySelector('.show-love-form');
 
-submitLove.addEventListener('click', getSubmissionCount);
+submitLove.addEventListener('click', submitForm);
 
 var ajax = new XMLHttpRequest();
 ajax.onload = getSubmissionCount;
 ajax.onerror = failedAttempt;
 ajax.open("GET", "https://api.netlify.com/api/v1/forms/5a3e7682494c5852683421de?access_token=976354873ebb7e74404f9fabe81e493ca236627a1d95c91b92577428aee803fe", true);
 ajax.send();
+
+
+function submitForm() {
+	showLoveForm.submit(function(e) {
+		e.preventDefault();
+		showLoveForm.innerHTML += '<input type='hidden' name='form-name' value='love' />';
+		const url = showLoveForm.getAttribute('action');
+		ajax.open('POST', 'url', true);
+		ajax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+		ajax.send("love");
+	});
+}
 
 function getSubmissionCount() {
 	if (this.status == 200) { // request succeeded
@@ -21,7 +34,7 @@ function getSubmissionCount() {
 }
 
 function failedAttempt(e) {
-	console.log(this);
-	console.error(e);
+	// console.log(this);
+	// console.error(e);
 	// do something with this.status, this.statusText
 }
