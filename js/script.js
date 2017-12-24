489492790876f420d0c668d8a90ed9d6e36972f5
Somiari Lucky <somiarilucky@gmail.com>
2017-12-24 12:21:18 +0100

    Revert to normal form submit to try ajax submit later again

--- js/script.js
+++ js/script.js
@@ -2,40 +2,12 @@ const loveCount = document.querySelector('.love-count');
 const submitLove = document.querySelector('.send-love-btn');
 const showLoveForm = document.querySelector('.show-love-form');
 
-submitLove.addEventListener('click', sendForm);
-// submitLove.addEventListener('submit', submitFunc);
-
 var ajax = new XMLHttpRequest();
 ajax.onload = getSubmissionCount;
 ajax.onerror = failedAttempt;
 ajax.open("GET", "https://api.netlify.com/api/v1/forms/5a3e7682494c5852683421de?access_token=976354873ebb7e74404f9fabe81e493ca236627a1d95c91b92577428aee803fe", true);
 ajax.send();
 
-function sendForm(e) {
-	e.preventDefault();
-
-	const xhr = new XMLHttpRequest();
-	const FD = new FormData();
-	// showLoveForm.innerHTML += "<input type='hidden' name='form-name' value='love' />";
-	const url = showLoveForm.getAttribute('action');
-
-	xhr.addEventListener('load', function (event) {
-		alert('Yeah! Data sent and response loaded.');
-	});
-
-	// Define what happens in case of error
-	xhr.addEventListener('error', function (event) {
-		alert('Oups! Something went wrong.');
-	});
-
-	xhr.open('POST', 'index', true);
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-	xhr.send(FD);
-}
-
-// function submitFunc() {
-// 	alert("Submitted!");
-// }
 
 function getSubmissionCount() {
 	if (this.status == 200) { // request succeeded
