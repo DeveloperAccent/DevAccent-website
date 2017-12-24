b188b815fd53404eac1717af57ae65169ecf226d
Somiari Lucky <somiarilucky@gmail.com>
2017-12-24 04:50:14 +0100

    Test ajax form submit

--- js/script.js
+++ js/script.js
@@ -17,8 +17,8 @@ function submitForm(e) {
 		ajax.open('POST', 'url', true);
 		ajax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 		ajax.onreadystatechange = function() {//Call a function when the state changes.
-			if(http.readyState == 4 && http.status == 200) {
-					alert(http.responseText);
+			if(ajax.readyState == 4 && ajax.status == 200) {
+					alert(ajax.responseText);
 			}}
 		ajax.send();
 }
