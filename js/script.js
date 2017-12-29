bb33330c75582ff96cf96c90eb5e76bcddd76014
Somiari Lucky <somiarilucky@gmail.com>
2017-12-29 09:11:16 +0100

    Test ajax like feature

--- js/script.js
+++ js/script.js
@@ -2,17 +2,30 @@ const loveCount = document.querySelector('.love-count');
 const submitLove = document.querySelector('.send-love-btn');
 const showLoveForm = document.querySelector('.show-love-form');
 
+// Add event listener for love btn
+submitLove.addEventListener('click', sendLove);
+
 var ajax = new XMLHttpRequest();
 ajax.onload = getSubmissionCount;
 ajax.onerror = failedAttempt;
 ajax.open("GET", "https://api.netlify.com/api/v1/forms/5a3e7682494c5852683421de?access_token=976354873ebb7e74404f9fabe81e493ca236627a1d95c91b92577428aee803fe", true);
 ajax.send();
 
+var xhr = new XMLHttpRequest();
+
+function sendLove(e) {
+	e.preventDefault();
+	var number;
+	var url = showLoveForm.getAttribute('action');
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+	xhr.send(number);
+}
 
 function getSubmissionCount() {
-	if (this.status == 200) { // request succeeded
+	if (ajax.status == 200) { // request succeeded
 		// do something with this.responseText;
-		var json = JSON.parse(this.responseText);
+		var json = JSON.parse(ajax.responseText);
 		var subCount = json.submission_count;
 		loveCount.innerText = subCount;
 	} else {
