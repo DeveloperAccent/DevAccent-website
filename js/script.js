18b382b43ecf03960359c1126f6d8e58344543e4
Somiari Lucky <somiarilucky@gmail.com>
2017-12-29 09:14:44 +0100

    Test ajax like feature

--- js/script.js
+++ js/script.js
@@ -20,6 +20,8 @@ function sendLove(e) {
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 	xhr.send(number);
+
+	getSubmissionCount();
 }
 
 function getSubmissionCount() {
